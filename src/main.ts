531928767eb16c85c25dbbec2a8688f7acf0e1fb
Luciane Santos <lucianesantcs@gmail.com>
2024-04-28 16:56:09 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import "./style.scss";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <main class="container">
+    <section class="content">
+      <h1>Vite + TS + Sass Boilerplate</h1>
+      <p>This is a boilerplate to create simple HTML CSS/SCSS TS pages.</p>
+      <h3>Before using:</h3>
+      <ul>
+        <li>If it's just a plain HTML CSS TS page, delete everything on <code>main.ts</code> file and use only the <code>index.html</code> file to structure your code.
+          <ul>
+            <li>
+              Start coding replacing div app with your HTML.
+            </li>
+            <li>You will have to import your <code>style.scss</code> and any other <code>.ts</code> file in the HTML</li>
+          </ul>
+        </li>
+        <li>If it's a more structured project that will use a lot of typescript, keep the structure as it is.</li>
+      </ul>
+    </section>
+    <footer class="footer">
+      <p>© <strong><a href="https://github.com/lucianesantcs">lucianesantcs</a></strong> 2024 /
+      Vite + TS + Sass Boilerplate v1.0.0</p>
+    </footer>
+  </main>
+`;
