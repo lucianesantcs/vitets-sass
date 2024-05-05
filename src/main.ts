ea1fad53285fa446d7058f8b0798351e31220c8b
Luciane Santos <lucianesantcs@gmail.com>
2024-05-05 12:54:39 -0300

    feat: configurado informações do projeto

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,30 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         </li>
         <li>If it's a more structured project that will use a lot of typescript, keep the structure as it is.</li>
       </ul>
+      <h3>Pre-settings:</h3>
+      <ul>
+        <li><a href="https://prettier.io/" target="_blank">Prettier</a></li>
+        <li><a href="https://eslint.org/" target="_blank">Eslint</a></li>
+        <li><a href="https://typicode.github.io/husky/" target="_blank">Husky</a></li>
+        <li><a href="https://github.com/lint-staged/lint-staged" target="_blank">Lint Staged</a></li>
+      </ul>
+      <h3>Run the project:</h3>
+      <ul>
+        <li>
+          Use Node.js v18 or higher
+          <ul>
+            <li><a href="https://nodejs.org/en/about/previous-releases">Older versions</a></li>
+          </ul>
+        </li>
+        <li>Run the command <code>npm install</code> to install the dependencies.</li>
+        <li>
+          Run the command <code>npm run dev</code> to install open the project.
+          <ul>
+            <li>Check the terminal to see the URL of the project. Eg: <strong>Local:http://localhost:5173/</strong></li>
+          </ul>
+        </li>
+      </ul>
+      <h3>Start coding! 🎉</h3>
     </section>
     <footer class="footer">
       <p>© <strong><a href="https://github.com/lucianesantcs">lucianesantcs</a></strong> 2024 /
